9eab1ad1fc460c4d26537e351801dd87005e669d
Levi Arcanjo <arcanjolevi@gmail.com>
2022-01-31 18:39:30 -0300

    graph representation on interface

--- frontend/src/components/Canvas.tsx
+++ frontend/src/components/Canvas.tsx
@@ -83,7 +83,7 @@ export const Canvas = () => {
       var width = canvas.attr("width");
       var height = canvas.attr("height");
       var ctx = canvas.node().getContext("2d");
-      var r = 10;
+      var r = 20;
       var simulation = d3
         .forceSimulation(data.nodes)
         .force("x", d3.forceX(width / 2))
@@ -158,11 +158,66 @@ export const Canvas = () => {
       function drawLink(l: DrawLinkProps) {
         ctx.beginPath();
         ctx.strokeStyle = l.color;
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 1;
         ctx.moveTo(l.source.x, l.source.y);
-        ctx.lineTo(l.target.x, l.target.y);
-        ctx.stroke();
+        var c = Math.abs(l.target.x - l.source.x);
+        var b = Math.abs(l.target.y - l.source.y);
 
+        var a = Math.sqrt(b * b + c * c);
+
+        var alph = (Math.asin(b / a) * 180) / Math.PI;
+
+        var betha = 90 - alph;
+
+        var yDislocationReal = Math.sin(alph) * r;
+        var xDislocationReal = Math.cos(alph) * r;
+        var xReal = 0;
+        var yReal = 0;
+
+        if (l.target.x > l.source.x) {
+          //origem esquerda
+
+          if (xDislocationReal < 0) {
+            xReal = l.target.x + xDislocationReal;
+          } else {
+            xReal = l.target.x - xDislocationReal;
+          }
+
+          if (yDislocationReal > 0) {
+            yReal = l.target.y + yDislocationReal;
+          } else {
+            yReal = l.target.y - yDislocationReal;
+          }
+        } else {
+          //origem direita
+
+          if (xDislocationReal >= 0) {
+            xReal = l.target.x + xDislocationReal;
+          } else {
+            xReal = l.target.x - xDislocationReal;
+          }
+
+          if (yDislocationReal <= 0) {
+            yReal = l.target.y + yDislocationReal;
+          } else {
+            yReal = l.target.y - yDislocationReal;
+          }
+        }
+
+        console.log(alph);
+
+        console.log("yReal: ", yDislocationReal);
+        // console.log("y: ", yReal);
+
+        canvas_arrow(ctx, l.source.x, l.source.y, xReal, yReal);
+
+        ctx.moveTo(l.source.x, l.source.y);
+        ctx.lineTo(l.target.x, l.source.y);
+
+        ctx.moveTo(l.target.x, l.target.y);
+        ctx.lineTo(l.target.x, l.source.y);
+
+        ctx.stroke();
         ctx.font = "10px serif";
         ctx.fillStyle = "purple";
         ctx.fillText(
@@ -172,17 +227,29 @@ export const Canvas = () => {
         );
       }
 
-      // function canvas_arrow(context, fromx, fromy, tox, toy) {
-      //   var headlen = 10; // length of head in pixels
-      //   var dx = tox - fromx;
-      //   var dy = toy - fromy;
-      //   var angle = Math.atan2(dy, dx);
-      //   context.moveTo(fromx, fromy);
-      //   context.lineTo(tox, toy);
-      //   context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
-      //   context.moveTo(tox, toy);
-      //   context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
-      // }
+      function canvas_arrow(
+        context: any,
+        fromx: any,
+        fromy: any,
+        tox: any,
+        toy: any
+      ) {
+        var headlen = 50; // length of head in pixels
+        var dx = tox - fromx;
+        var dy = toy - fromy;
+        var angle = Math.atan2(dy, dx);
+        context.moveTo(fromx, fromy);
+        context.lineTo(tox, toy);
+        context.lineTo(
+          tox - headlen * Math.cos(angle - Math.PI / 6),
+          toy - headlen * Math.sin(angle - Math.PI / 6)
+        );
+        context.moveTo(tox, toy);
+        context.lineTo(
+          tox - headlen * Math.cos(angle + Math.PI / 6),
+          toy - headlen * Math.sin(angle + Math.PI / 6)
+        );
+      }
     },
     [data.nodes.length]
   );
